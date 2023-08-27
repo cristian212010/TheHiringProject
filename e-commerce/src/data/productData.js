8f5bb12300ec31f130eddb84255605916b66dd0b
cristian212010 <cristiancamilo212010@gmail.com>
2023-08-27 15:47:05 -0500

    feat: :card_file_box: Array de datos para las Cards
    se creo un array que contiene los datos con lo que se rellenaran las tarjetas

---
+++ e-commerce/src/data/productData.js
@@ -0,0 +1,47 @@
+import printedTshirt from '../assets/img/printed-T-shirt.jpg';
+import runningShoes from '../assets/img/runningShoes.jpg';
+import leatherHandbag from '../assets/img/leatherHandbag.jpg';
+import wirelessEarbuds from '../assets/img/wirelessEarbuds.jpg';
+import smartWatch from '../assets/img/smartWatch.jpg';
+import denimJeans from '../assets/img/denimJeans.jpg';
+
+const products = [
+    {
+      title: 'Printed T-Shirt',
+      image: printedTshirt,
+      description:
+        'Soft cotton t-shirt with a unique printed design. Available in various sizes and colors.',
+    },
+    {
+      title: 'Running Shoes',
+      image: runningShoes,
+      description:
+        'Comfortable and stylish athletic shoes for all types of sports activities. Durable and breathable sole.',
+    },
+    {
+      title: 'Leather Handbag',
+      image: leatherHandbag,
+      description:
+        'Genuine leather handbag with multiple compartments and a modern design. Perfect for carrying.',
+    },
+    {
+      title: 'Wireless Earbuds',
+      image: wirelessEarbuds,
+      description:
+        'High-quality wireless earbuds with noise-cancellation technology. Crisp sound and comfortable fit.',
+    },
+    {
+      title: 'Smart Watch',
+      image: smartWatch,
+      description:
+        'Feature-rich smart watch with fitness tracking, notifications, and customizable watch faces.',
+    },
+    {
+      title: 'Denim Jeans',
+      image: denimJeans, 
+      description:
+        'Classic denim jeans made from premium denim fabric. Stylish and comfortable for any occasion.',
+    },
+];
+
+export default products;
